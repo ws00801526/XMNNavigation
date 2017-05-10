3dec20dd2b5bb6e092d0f9283c49516c7faef0cb
XMFraker <3057600441@qq.com>
2017-05-10 15:45:54 +0800

    增加viewController 样式配置功能

---
+++ Classes/XMNNavigation/UIViewController+XMNStyle.m
@@ -0,0 +1,20 @@
+//
+//  UIViewController+XMNStyle.m
+//  XMNNavigationExample
+//
+//  Created by XMFraker on 2017/5/10.
+//  Copyright © 2017年 XMFraker. All rights reserved.
+//
+
+#import "UIViewController+XMNStyle.h"
+
+@implementation UIViewController (XMNStyle)
+
+- (void)xmn_configControllerStyle {
+
+#if DEBUG
+    NSLog(@"config you controller style here");
+#endif
+}
+
+@end
